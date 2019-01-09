516ae2c3fbf82ff1103015d9bcab8ed156c7d12f
Kaustubh Srivastava <Kaustubh.Srivastava@studentpartner.com>
2019-01-09 09:25:51 +0530

    First

---
+++ CodeChallenge/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+
+namespace CodeChallenge.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Sends an OpenIDConnect Sign-In Request.  
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+
+                HttpContext.GetOwinContext()
+                    .Authentication.Challenge(new AuthenticationProperties { RedirectUri = "/" },
+                        OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+
+        // Signs the user out and clears the cache of access tokens.  
+        public void SignOut()
+        {
+
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType);
+
+        }
+
+
+    }
+}
